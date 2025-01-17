bdb8e493e02237f22998698a6c494f2b5d62fbe2
Juan Luna <juanlopez@princeton.edu>
2025-01-17 14:55:13 -0500

    Stop tracking .vuepress/.temp and ignore it

--- .vuepress/.temp/pages/maintenance/index.html.vue
+++
@@ -1,7 +0,0 @@
-<template><div><h1 id="frontmatter-title" tabindex="-1"><a class="header-anchor" href="#frontmatter-title"><span>{{ $frontmatter.title }}</span></a></h1>
-<h2 id="introduction" tabindex="-1"><a class="header-anchor" href="#introduction"><span>Introduction</span></a></h2>
-<p>This website is a central repository for the documentation regarding the <strong>maintenance</strong> of mini virtual reality rigs as part of BRAIN CoGS at Princeton Neuroscience Institute.</p>
-<p>Maintenance documentation is also divided into modules, each module contains the process and tools needed to perform both preventive and corrective maintenance, as well as common troubleshooting.</p>
-</div></template>
-
-
